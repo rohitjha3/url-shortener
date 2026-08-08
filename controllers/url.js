e42a024d2832b3518e5c16a8b042777fd1a31c12
Rohit Jha <rohitjha@Rohits-MacBook-Air.local>
2026-08-08 21:41:33 +0530

    Fix URL list after creation

--- controllers/url.js
+++ controllers/url.js
@@ -14,7 +14,7 @@ async function handleGenerateNewShortURL(req,res) {
     visitHistory: [],
     createdBy: req.user._id,
   })
- return res.render('home', { id: shortID});
+ return res.redirect("/");
   
 }
 
